d1a7f6d493504fe568bc606a1ca9e378c624d1c1
xofni <fakenewsProject@hotmail.com>
2019-01-15 21:30:33 +0100

    Updates

---
+++ precache-manifest.e869c9a8e8c39d6383f599a3a17eddb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ac3a92bf7d85bf6076688602e48485",
+    "url": "/observatoire/static/media/wdscroll_bas.99ac3a92.svg"
+  },
+  {
+    "revision": "f1116cf3063dc02de2cc",
+    "url": "/observatoire/static/js/runtime~main.f1116cf3.js"
+  },
+  {
+    "revision": "b6c892733ca26c5116ba",
+    "url": "/observatoire/static/js/main.b6c89273.chunk.js"
+  },
+  {
+    "revision": "0571de03744c57ecbf08",
+    "url": "/observatoire/static/js/1.0571de03.chunk.js"
+  },
+  {
+    "revision": "b6c892733ca26c5116ba",
+    "url": "/observatoire/static/css/main.a883fa36.chunk.css"
+  },
+  {
+    "revision": "d204aaefe58bfb3d3d4f1171420b524b",
+    "url": "/observatoire/index.html"
+  }
+];
