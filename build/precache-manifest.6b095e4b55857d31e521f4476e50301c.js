56b159e36ced5f7a5f93117a7ec5bed815fa6d1c
xofni <fakenewsProject@hotmail.com>
2020-06-05 11:22:35 +0200

    Rebuild package

---
+++ build/precache-manifest.6b095e4b55857d31e521f4476e50301c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ac3a92bf7d85bf6076688602e48485",
+    "url": "/observatoire/static/media/wdscroll_bas.99ac3a92.svg"
+  },
+  {
+    "revision": "f1116cf3063dc02de2cc",
+    "url": "/observatoire/static/js/runtime~main.f1116cf3.js"
+  },
+  {
+    "revision": "113ed57fa31b77ee5877",
+    "url": "/observatoire/static/js/main.113ed57f.chunk.js"
+  },
+  {
+    "revision": "0571de03744c57ecbf08",
+    "url": "/observatoire/static/js/1.0571de03.chunk.js"
+  },
+  {
+    "revision": "113ed57fa31b77ee5877",
+    "url": "/observatoire/static/css/main.01939292.chunk.css"
+  },
+  {
+    "revision": "7b928d33bb625f118f716a4e3206deb9",
+    "url": "/observatoire/index.html"
+  }
+];
