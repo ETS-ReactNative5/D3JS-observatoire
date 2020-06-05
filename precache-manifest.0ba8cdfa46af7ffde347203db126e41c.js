dd65b2de913d185deed7f8300683e0302a00ba8a
xofni <fakenewsProject@hotmail.com>
2020-06-05 11:57:34 +0200

    Updates

---
+++ precache-manifest.0ba8cdfa46af7ffde347203db126e41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ac3a92bf7d85bf6076688602e48485",
+    "url": "/D3JS-observatoire/static/media/wdscroll_bas.99ac3a92.svg"
+  },
+  {
+    "revision": "f7a2b523003d029bfc76",
+    "url": "/D3JS-observatoire/static/js/runtime~main.f7a2b523.js"
+  },
+  {
+    "revision": "113ed57fa31b77ee5877",
+    "url": "/D3JS-observatoire/static/js/main.113ed57f.chunk.js"
+  },
+  {
+    "revision": "0571de03744c57ecbf08",
+    "url": "/D3JS-observatoire/static/js/1.0571de03.chunk.js"
+  },
+  {
+    "revision": "113ed57fa31b77ee5877",
+    "url": "/D3JS-observatoire/static/css/main.01939292.chunk.css"
+  },
+  {
+    "revision": "4e3a9fc24da431f64748722ac8a2c474",
+    "url": "/D3JS-observatoire/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/observatoire/precache-manifest.6b095e4b55857d31e521f4476e50301c.js"
+  "/D3JS-observatoire/precache-manifest.0ba8cdfa46af7ffde347203db126e41c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/observatoire/index.html", {
+workbox.routing.registerNavigationRoute("/D3JS-observatoire/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
